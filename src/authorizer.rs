2cc2e09add9361ddc5823530431d9c5fcc2745ee
wangjun861205 <444055828@qq.com>
2022-04-12 13:48:12 +0800

    modify authorizer

--- src/authorizer.rs
+++ src/authorizer.rs
@@ -1,7 +1,12 @@
-use crate::diesel::{r2d2::ConnectionManager, PgConnection};
+use crate::diesel::{
+    dsl::{exists, select},
+    r2d2::ConnectionManager,
+    BoolExpressionMethods, ExpressionMethods, PgConnection, QueryDsl, RunQueryDsl,
+};
 use crate::error::Error;
 use crate::handlers::authorizer::Authorizer;
 use crate::r2d2::Pool;
+use crate::schema::*;
 
 type ConnectionPool = Pool<ConnectionManager<PgConnection>>;
 
@@ -17,21 +22,59 @@ impl PgAuthorizer {
 
 impl Authorizer for PgAuthorizer {
     fn check_organization_read(&self, uid: i32, org_id: i32) -> Result<bool, Error> {
-        Ok(true)
+        let conn = self.pool.get()?;
+        let is_exists = select(exists(
+            users_organizations::table.filter(users_organizations::user_id.eq(uid).and(users_organizations::organization_id.eq(org_id))),
+        ))
+        .get_result(&conn)?;
+        Ok(is_exists)
     }
     fn check_organization_write(&self, uid: i32, org_id: i32) -> Result<bool, Error> {
-        Ok(true)
+        let conn = self.pool.get()?;
+        select(exists(
+            users_organizations::table.filter(users_organizations::user_id.eq(uid).and(users_organizations::organization_id.eq(org_id))),
+        ))
+        .get_result(&conn)
+        .map_err(|e| Error::from(e))
     }
     fn check_vote_read(&self, uid: i32, vote_id: i32) -> Result<bool, Error> {
-        Ok(true)
+        let conn = self.pool.get()?;
+        select(exists(
+            users::table
+                .inner_join(users_organizations::table.inner_join(organizations::table.inner_join(votes::table)))
+                .filter(users::id.eq(uid).and(votes::id.eq(vote_id))),
+        ))
+        .get_result(&conn)
+        .map_err(|e| Error::from(e))
     }
     fn check_vote_write(&self, uid: i32, vote_id: i32) -> Result<bool, Error> {
-        Ok(true)
+        let conn = self.pool.get()?;
+        select(exists(
+            users::table
+                .inner_join(users_organizations::table.inner_join(organizations::table.inner_join(votes::table)))
+                .filter(users::id.eq(uid).and(votes::id.eq(vote_id))),
+        ))
+        .get_result(&conn)
+        .map_err(|e| Error::from(e))
     }
-    fn check_question_read(&self, uid: i32, question_id: i32) -> Result<bool, Error> {
-        Ok(true)
+    fn check_question_read(&self, uid: i32, qst_id: i32) -> Result<bool, Error> {
+        let conn = self.pool.get()?;
+        select(exists(
+            users::table
+                .inner_join(users_organizations::table.inner_join(organizations::table.inner_join(votes::table.inner_join(questions::table))))
+                .filter(users::id.eq(uid).and(questions::id.eq(qst_id))),
+        ))
+        .get_result(&conn)
+        .map_err(|e| Error::from(e))
     }
-    fn check_question_write(&self, uid: i32, question_id: i32) -> Result<bool, Error> {
-        Ok(true)
+    fn check_question_write(&self, uid: i32, qst_id: i32) -> Result<bool, Error> {
+        let conn = self.pool.get()?;
+        select(exists(
+            users::table
+                .inner_join(users_organizations::table.inner_join(organizations::table.inner_join(votes::table.inner_join(questions::table))))
+                .filter(users::id.eq(uid).and(questions::id.eq(qst_id))),
+        ))
+        .get_result(&conn)
+        .map_err(|e| Error::from(e))
     }
 }
